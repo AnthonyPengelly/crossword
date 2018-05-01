b74969b85ebfa7b6d590e81100c4ee127129c5c0
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-05-01 22:16:40 +0100

    Re-add access key info and fix bash script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,10 @@
 # Run from project root.
 npm install 
 
-if [[ $TRAVIS_BRANCH == 'master' ]]
-then
+if  [ $TRAVIS_BRANCH = "master" ]; then
     ENVIRONMENT=test npm run build
 fi
 
-if [[ $TRAVIS_BRANCH == 'live' ]]
-then
+if [ $TRAVIS_BRANCH = "live" ]; then
     ENVIRONMENT=live npm run build
 fi
